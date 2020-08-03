f7370a1591c529490f492d3d38c44df6ca6adb65
rpehkone <pehkonenroni@gmail.com>
2020-08-03 23:20:55 +0300

    Moved initialization function from fractal to main

--- src/main.c
+++ src/main.c
@@ -12,6 +12,78 @@
 
 #include "fractal.h"
 
+void	*split_screen(void *args)
+{
+	static int			start = 0;
+	static int			stop = 0;
+	struct sched_param	params;
+
+	params.sched_priority = sched_get_priority_max(SCHED_FIFO);
+	pthread_setschedparam(pthread_self(), SCHED_FIFO, &params);
+	stop += HEIGHT / THREAD_COUNT;
+	if (((t_args*)args)->fractal_id == 1)
+		mandelbrot((t_args*)args, start, stop);
+	else if (((t_args*)args)->fractal_id == 2)
+		julia((t_args*)args, start, stop);
+	else if (((t_args*)args)->fractal_id == 3)
+	{
+		((t_args*)args)->color = 6;
+		((t_args*)args)->pos.x = -175;
+		((t_args*)args)->pos.y = -3;
+		((t_args*)args)->zoom = 0.0117;
+		((t_args*)args)->pos2.x = -175;
+		((t_args*)args)->pos2.y = -3;
+		((t_args*)args)->zoom2 = 0.0117;
+		burningship((t_args*)args, start, stop);
+	}
+	start += HEIGHT / THREAD_COUNT;
+	return (NULL);
+}
+
+void	init_args(t_args *args)
+{
+	args->pos.x = 0;
+	args->pos.y = 0;
+	args->pos2.x = 0;
+	args->pos2.y = 0;
+	args->zoom2 = .3;
+	args->max_iter2 = 50;
+	args->zoom = .3;
+	args->max_iter = 50;
+	args->sync_threads = 1;
+	args->buffer_id = 0;
+	args->threads_ready = 0;
+	args->color = 5;
+}
+
+t_args	*init_fractal(int f)
+{
+	int				i;
+	pthread_t		tid[THREAD_COUNT];
+	static t_args	args;
+
+	init_args(&args);
+	if (f)
+	{
+		args.fractal_id = f;
+		return (NULL);
+	}
+	if (!(args.dbuffer = (int**)malloc(sizeof(int*) * 2)))
+		exit(0);
+	if (!(args.dbuffer[0] = (int*)malloc(sizeof(int) * (WIDTH * HEIGHT))))
+		exit(0);
+	if (!(args.dbuffer[1] = (int*)malloc(sizeof(int) * (WIDTH * HEIGHT))))
+		exit(0);
+	i = 0;
+	while (i < THREAD_COUNT)
+	{
+		pthread_create(&tid[i], NULL, split_screen, (void*)&args);
+		usleep(10000);
+		i++;
+	}
+	return (&args);
+}
+
 void	put_usage(char *arg)
 {
 	ft_putstr("usage: ");
